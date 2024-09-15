9f092c0be63d70538934d900729773ba5b966d8d
KASAFA <salamfawaz667@gmail.com>
2024-09-15 12:14:15 +0800

    file pertama

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const saldo = document.getElementById("tambah")
+const tomboTambah = document.getElementById("tombol-tambah")
+
+tomboTambah.addEventListener("click", function() {
+    let pemasukan = saldo.value
+    
+    console.log(pemasukan)
+})
