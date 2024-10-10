87d6ef759431ec01d3e47a2706330ec0c83d7f04
KASAFA <salamfawaz667@gmail.com>
2024-10-10 11:02:38 +0800

    add title and hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
-import * as fbFunctions from './firebase.js';
-console.log(fbFunctions.buah)
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('navMenu');
+
+    hamburger.addEventListener('click', () => {
+        // Toggle menu visibility
+        navMenu.classList.toggle('open');
+        
+        // Toggle hamburger icon between "hamburger" and "X"
+        if (navMenu.classList.contains('open')) {
+            hamburger.innerHTML = '&#10005;'; // Unicode for "X"
+        } else {
+            hamburger.innerHTML = '&#9776;'; // Unicode for hamburger
+        }
+    });
+});
